d90de084f9e93964de0898284000aa84cf3eb9a2
emily <emilyglover99@yandex.com>
2025-12-03 11:47:27 +0000

    day 3 part 2 not quite working

--- 2025/Day 3/main.py
+++ 2025/Day 3/main.py
@@ -4,13 +4,55 @@ import itertools
 with open('./batteries.txt', 'r') as f:
         batteries_list = f.read().split('\n')
 
-bank_values = []
+# bank_values = []
+#
+# for bank in batteries_list:
+#     two_sets = itertools.combinations(list(map(int, str(bank))), 2)
+#     combined = [int(str(x) + str(y)) for (x, y) in two_sets]
+#     bank_values.append(max(combined))
+#
+# print(bank_values)
+# sum = int(sum(bank_values))
+# print(sum)
+
+# Part 2
+
+final_numbers = []
 
 for bank in batteries_list:
-    two_sets = itertools.combinations(list(map(int, str(bank))), 2)
-    combined = [int(str(x) + str(y)) for (x, y) in two_sets]
-    bank_values.append(max(combined))
+    # Take the last 12 digits to be the first try:
+    final_number = list(bank[-12:])
+    final_number_indices = []
 
-print(bank_values)
-sum = int(sum(bank_values))
-print(sum)
+
+    for i in range(0, 12):
+        # Find the next best value for each digit
+        if i == 0:
+            remaining_digits = bank[:len(bank) - 12]
+            largest_remaining_digit = max(remaining_digits)
+            index_of_first_largest_digit = remaining_digits.index(largest_remaining_digit)
+            final_number[i] = largest_remaining_digit
+            final_number_indices.append(index_of_first_largest_digit)
+        else:
+            print("Digit number: ", i)
+            remaining_digits = bank[final_number_indices[i - 1] + 1: (len(bank) - 12) + i + 1]
+            print("Remaining digits: ", remaining_digits)
+            largest_remaining_digit = max(remaining_digits)
+            print("Largest remaining digit: ", largest_remaining_digit)
+            print("Index of largest remaining digit: ", remaining_digits.index(largest_remaining_digit))
+            index_of_first_largest_digit = remaining_digits.index(largest_remaining_digit) + int(final_number_indices[i - 1] + 1)
+            print(index_of_first_largest_digit)
+
+            final_number[i] = largest_remaining_digit
+            final_number_indices.append(index_of_first_largest_digit)
+
+    combined = int("".join(map(str, final_number)))
+    print("Final combined number: ", combined)
+
+    final_numbers.append(combined)
+
+print(final_numbers)
+sum(final_numbers)
+
+
+
